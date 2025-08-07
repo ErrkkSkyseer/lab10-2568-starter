c458ecf14fbb3bb19621176c62778755c0bbe3ac
ErrkkSkyseer <lolplayer78876@gmail.com>
2025-08-07 12:30:37 +0700

    patch 2

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { UserCardDetail } from "./UserCardDetail";
 import { BsChevronUp, BsChevronDown } from "react-icons/bs";
-export const UserCard = ({ name, imgUrl, address, email }) => {
+export const UserCard = ({ _name, _imgUrl, _address, _email }) => {
   const [isDetailShown, setIsDetailShown] = useState(false);
 
   const userCardOnClick = () => {
@@ -11,11 +11,11 @@ export const UserCard = ({ name, imgUrl, address, email }) => {
   return (
     <div className="border-bottom">
       <div className="d-flex align-items-center p-3" onClick={userCardOnClick}>
-        <img src={imgUrl} width="90px" className="rounded-circle me-4"></img>
-        <span className="text-center display-6 me-auto">{name}</span>
+        <img src={_imgUrl} width="90px" className="rounded-circle me-4"></img>
+        <span className="text-center display-6 me-auto">{_name}</span>
         {isDetailShown ? <BsChevronUp /> : <BsChevronDown />}
       </div>
-      {isDetailShown && <UserCardDetail email={email} address={address} />}
+      {isDetailShown && <UserCardDetail email={_email} address={_address} />}
     </div>
   );
 };
